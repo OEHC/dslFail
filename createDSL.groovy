aad869295bed122aae89abd3f51a4537457bb2ff
Oliver Emil Harritslev Christensen <oehc@praqma.net>
2017-01-05 13:04:26 +0100

    Create DSL from YAML

---
+++ createDSL.groovy
@@ -0,0 +1,23 @@
+package dsl
+
+import org.yaml.snakeyaml.Yaml
+
+String configFile = new File("config.yaml").text
+
+Yaml yaml = new Yaml()
+Map map = (Map) yaml.load(configFile)
+
+String jobName = map.get("jobName")
+
+println """job("${jobName}") {
+    scm {
+        git("https://github.com/OEHC/dsl", "*/master")
+    }
+
+    triggers {
+        scmTrigger {
+            scmpoll_spec("")
+            ignorePostCommitHooks(false)
+        }
+    }
+}"""
