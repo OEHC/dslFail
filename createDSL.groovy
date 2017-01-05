dc0a514b8b57ecc4760d2d4826a61dce8547af95
Oliver Emil Harritslev Christensen <oehc@praqma.net>
2017-01-05 15:44:43 +0100

    wip

--- createDSL.groovy
+++ createDSL.groovy
@@ -5,33 +5,32 @@ import org.yaml.snakeyaml.Yaml
 
 Yaml yaml
 String jobName
+for (LinkedHashMap m : readYaml()) {
+    jobName = m.jobName
 
-String dir = 'dirToLookAt'
+    job("${jobName}") {
+        scm {
+            git("https://github.com/OEHC/dsl", "*/master")
+        }
 
-new File(__FILE__).parentFile.eachFileRecurse {
-    if(it.name.endsWith("yaml")) {
-        yaml = new Yaml()
-        for (LinkedHashMap m : yaml.loadAll(readFileFromWorkspace(it.absolutePath))) {
-            println m
-            jobName = m.jobName
-
-            job("${jobName}") {
-                scm {
-                    git("https://github.com/OEHC/dsl", "*/master")
-                }
-
-                triggers {
-                    scmTrigger {
-                        scmpoll_spec("")
-                        ignorePostCommitHooks(false)
-                    }
-                }
+        triggers {
+            scmTrigger {
+                scmpoll_spec("")
+                ignorePostCommitHooks(false)
             }
         }
     }
 }
 
-private LinkedHashMap[] readYaml() {
-
-    return new LinkedHashMap[1]
+private ArrayList readYaml() {
+    ArrayList ret = new ArrayList()
+    new File(__FILE__).parentFile.eachFileRecurse {
+        if (it.name.endsWith("yaml")) {
+            yaml = new Yaml()
+            for (LinkedHashMap m : yaml.loadAll(readFileFromWorkspace(it.absolutePath))) {
+                ret.add(m)
+            }
+        }
+    }
+    return ret
 }
